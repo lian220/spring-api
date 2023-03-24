bd77810a9afb005b4ba25b9fa2d4ac822e051331
ygchoi <ygchoi@kcp.co.kr>
2023-03-24 15:43:40 +0900

    ExceptHandler 추가

--- src/main/java/choi/web/api/domain/ResponseData.java
+++ src/main/java/choi/web/api/domain/ResponseData.java
@@ -1,13 +1,17 @@
 package choi.web.api.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL) // Null 값인 필드 제외
 public class ResponseData {
 
     String resultCode;
+
+    String resultMessage;
 
     Object resultData;
 
